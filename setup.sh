59981244d4a80fd96213e8bdaddeced6d5d771b2
Lawrence <lawrence.mk.johny@gmail.com>
2021-07-02 07:40:58 +0100

    heroku files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"lawrence.mk.johny@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
